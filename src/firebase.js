1b773097834e534a44ec41b5194614ebc4e6ef4c
Cosmo Kent <cosmolightfoot@gmail.com>
2019-03-11 11:34:47 -0700

    config files set up

---
+++ src/firebase.js
@@ -0,0 +1,7 @@
+const config = {
+    apiKey: "AIzaSyBa3U-OUQ36TVqINC99IqnwXMM5l3loeUI",
+    authDomain: "user-auth-group-mob.firebaseapp.com"
+
+};
+firebase.initializeApp(config);
+export const auth = firebase.auth();
