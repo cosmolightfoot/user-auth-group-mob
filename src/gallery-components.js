2ab87c9fd08e73959c22520a05eb3de34b1d12b9
Cosmo Kent <cosmolightfoot@gmail.com>
2019-03-12 15:58:59 -0700

    take break, middle of adding favorite star functionality

--- src/gallery-components.js
+++ src/gallery-components.js
@@ -3,7 +3,7 @@ import { auth, favoritesByUserRef } from '../src/firebase.js';
 export function makeHtmlTemplate(character){
     const html= /*html*/`
         <li class="card">
-        <span class="favorite">☆</span>
+        <span class="favorite-star">☆</span>
             <h1>${character.name}</h1>
             <img src="${character.image}">
             <h2>Status: ${character.status}</h2>
@@ -21,11 +21,37 @@ export default function loadGallery(characterArray) {
     clearRows();
     characterArray.forEach(character => {
         const dom = makeHtmlTemplate(character);
-        const favoriteStar = dom.querySelector('.favorite');
+        const favoriteStar = dom.querySelector('.favorite-star');
         favoriteStar.addEventListener('click', () => {
             const userId = auth.currentUser.uid;
             const userFavoritesRef = favoritesByUserRef.child(userId);
             const userFavoriteCharacterRef = userFavoritesRef.child(character.id);
+            userFavoriteCharacterRef.once('value')
+                .then(snapshot => {
+                    const value = snapshot.val();
+                    let isFavorite = false;
+                    if(value) {
+                        addFavorite();
+                    }
+                    else {
+                        removeFavorite();
+                    }
+
+                    function addFavorite() {
+                        isFavorite = true;
+                        favoriteStar.textContent = '★';
+                        favoriteStar.classList.add('favorite');
+                    }
+
+                    function removeFavorite() {
+                        isFavorite = false;
+                        favoriteStar.textContent = '☆';
+                        favoriteStar.classList.remove('favorite');
+                    }
+                    //LINE 63 favoriteStar.addEVLIS
+                    //https://github.com/alchemy-web-bootcamp-winter-2019/movie-db-example/blob/14646d484fb25aa928cd2a9ca8b73351d295c703/src/movies/movies-component.js
+
+                });
             userFavoriteCharacterRef.set({
                 id: character.id,
                 title: character.name,
