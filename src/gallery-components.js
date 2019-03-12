7b19a9bd5a8f55774a30df62221b938cd15fd9fb
Cosmo Kent <cosmolightfoot@gmail.com>
2019-03-12 15:39:53 -0700

    logs user favorites on star click

--- src/gallery-components.js
+++ src/gallery-components.js
@@ -1,6 +1,9 @@
+import { auth, favoritesByUserRef } from '../src/firebase.js';
+
 export function makeHtmlTemplate(character){
     const html= /*html*/`
         <li class="card">
+        <span class="favorite">☆</span>
             <h1>${character.name}</h1>
             <img src="${character.image}">
             <h2>Status: ${character.status}</h2>
@@ -18,7 +21,22 @@ export default function loadGallery(characterArray) {
     clearRows();
     characterArray.forEach(character => {
         const dom = makeHtmlTemplate(character);
+        const favoriteStar = dom.querySelector('.favorite');
+        favoriteStar.addEventListener('click', () => {
+            const userId = auth.currentUser.uid;
+            const userFavoritesRef = favoritesByUserRef.child(userId);
+            const userFavoriteCharacterRef = userFavoritesRef.child(character.id);
+            userFavoriteCharacterRef.set({
+                id: character.id,
+                title: character.name,
+                image: character.image,
+                status: character.status,
+                species: character.species
+            });
+        });
         characterList.appendChild(dom);
+
+
     });
 }
 
