628c5584ca1ae7c7246442bce9f4a7b5de53b30e
Cosmo Kent <cosmolightfoot@gmail.com>
2019-03-12 13:50:16 -0700

    added db url in firebasejs

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,12 @@
 const config = {
     apiKey: "AIzaSyBa3U-OUQ36TVqINC99IqnwXMM5l3loeUI",
-    authDomain: "user-auth-group-mob.firebaseapp.com"
+    authDomain: "user-auth-group-mob.firebaseapp.com",
+    databaseURL: "https://user-auth-group-mob.firebaseio.com",
+    projectId: "user-auth-group-mob"
+ 
 
 };
 firebase.initializeApp(config);
 export const auth = firebase.auth();
+const db = firebase.database();
+export const favoritesByUserRef = db.ref('favorites-by-user');
